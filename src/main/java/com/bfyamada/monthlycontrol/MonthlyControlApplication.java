0ef62aca7059c1e4ba8b447bf6137d49ee9aa3a1
Bruno Yamada <bfyamada@outlook.com>
2021-07-09 20:39:33 -0300

    First Commit

---
+++ src/main/java/com/bfyamada/monthlycontrol/MonthlyControlApplication.java
@@ -0,0 +1,13 @@
+package com.bfyamada.monthlycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfyamada/monthlycontrol/MonthlyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfyamada.monthlycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
